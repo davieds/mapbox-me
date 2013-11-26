74af1890c4715f18c9a3743f8aafa31d2fd7b23a
Justin R. Miller <incanus@codesorcery.net>
2013-11-26 15:41:17 -0700

    basic content view

--- Infinity Scroll/ISViewController.m
+++ Infinity Scroll/ISViewController.m
@@ -8,15 +8,24 @@
 
 #import "ISViewController.h"
 
-@interface ISViewController ()
-
-@end
-
 @implementation ISViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    self.view.backgroundColor = [UIColor lightGrayColor];
+
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake((self.view.bounds.size.width - 512) / 2, (self.view.bounds.size.height - 512) / 2, 512, 512)];
+    scrollView.contentSize = CGSizeMake(1024, 1024);
+    scrollView.scrollEnabled = YES;
+    scrollView.bounces = NO;
+    scrollView.backgroundColor = [UIColor darkGrayColor];
+    [self.view addSubview:scrollView];
+
+    UIView *contentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1024, 1024)];
+    contentView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"LoadingTile6.png"]];
+    [scrollView addSubview:contentView];
 }
 
 @end
